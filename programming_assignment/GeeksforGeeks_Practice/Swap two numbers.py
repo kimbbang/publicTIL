ae65938c1fba8dc5d57eb7265a74d4ad701a9f75
kimbbang <kentucky@skku.edu>
2022-05-23 20:00:17 +0900

    Swap two numbers.py

--- programming_assignment/GeeksforGeeks_Practice/Swap two numbers.py
+++ programming_assignment/GeeksforGeeks_Practice/Swap two numbers.py
@@ -25,3 +25,17 @@ if __name__ == "__main__":
         ans=ob.get(a,b)
         print(str(ans[0])+" "+str(ans[1]))
 #} Driver Code Ends
+
+##################### ▼ Author's Solution ▼ #####################
+# Python3 code to swap using XOR
+x = 10
+y = 5
+
+# Code to swap 'x' and 'y'
+x = x ^ y; # x now becomes 15 (1111)
+y = x ^ y; # y becomes 10 (1010)
+x = x ^ y; # x becomes 5 (0101)
+
+print ("After Swapping: x = ", x, " y =", y)
+# This code is contributed by
+# Sumit Sudhakar
